6ea22d168f3848364a4ac5691677b0c978304fba
shobhitsundriyal <shobhitsundriyal0170@gmail.com>
2023-02-06 22:22:54 +0530

    explore page fixed

--- pages/explore.js
+++ pages/explore.js
@@ -1,21 +1,28 @@
 import DatasetCard from '@/components/DatasetCard'
-import React from 'react'
+import { useContract } from '@/contexts/ContractContext'
+import React, { useEffect, useState } from 'react'
+import lodash from 'lodash'
 
 const explore = () => {
 	//get all datasets
+	const { marketplace } = useContract()
+	const [total, setTotal] = useState(0)
+
+	useEffect(() => {
+		if (marketplace) {
+			async function getData() {
+				const latestId = await marketplace.latestDataId()
+				setTotal(parseInt(latestId._hex, 16))
+			}
+			getData()
+		}
+	}, [marketplace])
+
 	return (
 		<div className='grid grid-cols-3 brp:grid-cols-4 gap-y-6 pt-5 pl-5'>
-			<DatasetCard id={1} />
-			<DatasetCard />
-			<DatasetCard />
-			<DatasetCard />
-			<DatasetCard /> <DatasetCard />
-			<DatasetCard />
-			<DatasetCard />
-			<DatasetCard /> <DatasetCard />
-			<DatasetCard />
-			<DatasetCard />
-			<DatasetCard />
+			{lodash.times(total, lodash.constant(null)).map((item, index) => (
+				<DatasetCard id={index + 1} key={index} />
+			))}
 		</div>
 	)
 }
